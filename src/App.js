a74d25a597f09b0d1bf44774bd1b61870fab3af0
yeisas <jeezluck@gmail.com>
2022-02-12 04:28:55 +0100

    change component dependency

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Component } from 'react/cjs/react.production.min';
+import { Component } from 'react';
 import logo from './logo.svg';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import './App.css';
