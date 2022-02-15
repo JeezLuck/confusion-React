8b0ebd8fc37ceab9642db50f724d491fe9d6f03c
yeisas <jeezluck@gmail.com>
2022-02-15 21:39:56 +0100

    React Router

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Navbar, NavbarBrand } from 'reactstrap';
 import './App.css';
 import { DISHES } from './shared/dishes';
 import Main from './components/MainComponent';
+import { BrowserRouter } from 'react-router-dom';
 
 
 class App extends Component {
@@ -17,9 +18,11 @@ class App extends Component {
 
   render() {
     return (
+      <BrowserRouter>
       <div className="App">
         <Main />
       </div>
+    </BrowserRouter>
     );
   }
 }
